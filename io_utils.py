84552eea60ea8d22936ef8c88c04c7dac3911c28
xuhuali-mxj <62774150+xuhuali-mxj@users.noreply.github.com>
2025-08-27 22:22:27 +0300

    Update io_utils.py

--- io_utils.py
+++ io_utils.py
@@ -1,9 +1,9 @@
 import argparse
 import backbone
 
-# model_dict = dict(ResNet10 = backbone.ResNet10)
-#model_dict = dict(ResNet18 = backbone.ResNet18)
-model_dict = dict(ViT = backbone.vit)
+model_dict = dict(ResNet10 = backbone.ResNet10)
+# model_dict = dict(ResNet18 = backbone.ResNet18)
+# model_dict = dict(ViT = backbone.vit)
 
 def parse_args(script):
     parser = argparse.ArgumentParser(description= 'few-shot script %s' %(script))
